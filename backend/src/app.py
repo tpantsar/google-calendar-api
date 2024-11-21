b9cc6acd3f96ee2c41dc20b7c73dd653086b3164
tpantsar <tomi.pantsar@gmail.com>
2024-11-21 18:35:43 +0200

    delete render templates

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, Response, render_template, request
+from flask import Flask, Response, request
 
 from constants import JSON, MASON
 from logger_config import logger
@@ -18,31 +18,6 @@ app = Flask(__name__)
 
 # Set Google API scopes
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
-
-
-@app.route("/year/<int:year>/<calendar_id>")
-def year_view(year, calendar_id):
-    """Displays the year view with calendar events."""
-    events = get_calendar_events(calendar_id, year)
-    service = build_service()
-
-    calendar = service.calendars().get(calendarId=calendar_id).execute()
-    calendar_name = calendar.get("summary")
-    logger.info(f"Displaying the year view for {calendar_name} for the year {year}")
-
-    # Pass events to the template
-    return render_template(
-        "calendar_view.html", calendar=calendar_name, events=events, year=year
-    )
-
-
-@app.route("/calendars")
-def calendars_view():
-    """Displays the list of calendars."""
-    calendars = get_calendar_list()
-
-    # Pass calendars to the template
-    return render_template("calendar_list.html", calendars=calendars)
 
 
 @app.get("/api/calendars")
