d431958f79763f7e403aca3f476d83cee037796f
tpantsar <tomi.pantsar@gmail.com>
2024-11-23 14:50:48 +0200

    configure CORS on backend

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 from api import api_bp
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all domains on all routes
+
+# Enable CORS for specific routes
+# CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 app.register_blueprint(api_bp)
 
 if __name__ == "__main__":
