4faaa21a7bb0a93773473b6ee543852791d50b3a
tpantsar <tomi.pantsar@gmail.com>
2024-12-26 16:12:37 +0200

    check if credentials.json file is not found

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
 from api import api_bp
+from logger_config import logger
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all domains on all routes
@@ -10,6 +13,13 @@ CORS(app)  # Enable CORS for all domains on all routes
 # CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(api_bp)
+
+if not os.path.exists("credentials.json"):
+    logger.error("credentials.json file not found.")
+    logger.error("Please download the credentials.json file from Google Cloud Console.")
+    logger.error("Place the file in the backend/src directory.")
+    logger.error("Refer to the README.md file for more information.")
+    exit(1)
 
 if __name__ == "__main__":
     app.run(debug=True)
